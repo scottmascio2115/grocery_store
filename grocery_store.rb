2aad8d3d9ead805fcaf86d356e472db6b530a39a
Scott R. Mascio <ScottMascio@Scotts-MacBook-Pro.local>
2013-08-31 11:54:16 -0500

    .

--- grocery_store.rb
+++ grocery_store.rb
@@ -1,3 +1,4 @@
+#
 require 'csv'
 require_relative 'aisles.rb'
 
@@ -12,21 +13,21 @@ class Grocery_Store
 		@capacity = @customer_data_base.length
 		@capacity_max = 20000
 	end
- 
+
 	def load_employees(file)
 		CSV.foreach(file, :headers => true, :header_converters => :symbol) do |row|
 			@employee_data_base << Employee.new(row)
 		end
 		@employee_data_base
 	end
- 
+
 	def load_customers(file)
 		CSV.foreach(file, :headers => true, :header_converters => :symbol)do |row|
 			@customer_data_base << Customer.new(row)
 		end
 		@customer_data_base
 	end
- 
+
 	def full_store_description
 		<<-STRING
 							STORE
@@ -39,56 +40,56 @@ class Grocery_Store
 		___________________________
 		STRING
 	end
- 
+
 	def employee_hired(name)
 		@employee_data_base << Employee.new(name)
 	end
- 
+
 	def employee_fired(id)
 		index = @employee_data_base.find_index {|x| x.id == id}
 		@employee_data_base.delete_at(index)
 	end
- 
+
 	def employee_on_shift(id)
 	  @employee_data_base.select {|x| x.on_shift = "ON" if x.id == id}
 	end
- 
- 
+
+
 	def employee_off_shift(id)
 		@employee_data_base.select {|x| x.on_shift = "OFF" if x.id == id}
 	end
- 
+
 	def employee_full_description
 		@employee_data_base.each {|x| puts x.full_employee_description}
 	end
- 
+
 	def customer_full_description
 		@customer_data_base.each {|x| puts x.full_customer_description}
 	end
- 
+
 	def customer_enters(name)
 		@customer_data_base << Customer.new(name)
 		if @capacity >= @capacity_max
 			raise "Store at capacity"
 		end
 	end
- 
+
 	def count_customers
 		@customer_data_base.length
 	end
- 
+
 	def list_customers
 		@customer_data_base.each {|x| puts x.first_name}
 	end
- 
+
 	def customer_leaves(id)
 		index = @customer_data_base.find_index {|x| x.id == id}
 		@customer_data_base.delete_at(index)
 	end
- 
+
 end
 
- 
+
 class Employee
 	attr_reader :first_name, :last_name, :id, :on_shift
 	attr_accessor :on_shift
@@ -98,7 +99,7 @@ class Employee
 		@last_name = args[:employee_last_name]
 		@on_shift = args[:on_shift]
 	end
- 
+
 	def full_employee_description
 		<<-STRING
 							EMPLOYEE
@@ -109,10 +110,10 @@ class Employee
 		___________________________
 		STRING
 	end
- 
+
 end
- 
- 
+
+
 class Customer
 	attr_reader :first_name, :last_name, :age, :id
 	def initialize(args)
@@ -120,9 +121,9 @@ class Customer
 		@first_name = args[:customer_first_name]
 		@last_name = args[:customer_last_name]
 		@age = args[:age]
- 
+
 	end
- 
+
 	def full_customer_description
 		<<-STRING
 							CUSTOMER
@@ -133,7 +134,7 @@ class Customer
 		___________________________
 		STRING
 	end
- 
+
 end
 
 derrick = Employee.new({:employee_first_name => "Derrick", :employee_last_name => "Rose", :id => "1"})
