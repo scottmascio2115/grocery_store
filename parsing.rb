26b054959669e8270a68639f34b0dada5ca7aa46
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-02 08:40:56 -0500

    .

--- parsing.rb
+++ parsing.rb
@@ -3,3 +3,19 @@ require 'Nokogiri'
 doc = Nokogiri::HTML(open("http://www.grantland.com/"))
 p doc.search('ul.list-content').inner_text
 
+=begin save feature in csv
+ def save
+    for_csv = []
+    @new_ppl.each do |person|
+      for_csv << "#{person.id},#{person.first_name},#{person.last_name},#{person.email},#{person.phone},#{person.created_at}"
+    end
+ 
+    CSV.open(@file, "a") do |csv|
+      @new_ppl.each do |person|
+        csv << for_csv
+      end
+    end
+  end
+ 
+end
+=end
